0c159c4d030354fdf5659433edbc316c52ac2cfb
Apfelkuchen91 <Apfelkuchen91>
2013-05-21 13:02:41 +0200

    No more global configuration for eclipse

--- src/start.cpp
+++ src/start.cpp
@@ -1,10 +1,28 @@
 /*!
+ * @mainpage Tetris
+ *
+ * @author DHBW Karslruhe TINF12B5 Tetris Gruppe
+ *
+ * @section sec1 Ziel des Projektes
+ * Lernen der Programmiersprache C++.
+ *
+ * @subsection sec1_1 Vorsaussetzung
+ * Grundlagen in C.
+ *
+ * @section doku Dokumentation
+ * Dokumentation wird mit hilfe von Doxygen automatisch erstellt. Bitte die Dokumentation von Doxygen f&uuml;r die Kommentare beachten und auch verwenden.
+ */
+
+
+
+
+/*!
  * @file	start.cpp
  * @date	21.05.2013
  * @author	Marcel Sutter
  *
  * @brief	Keine Klasse, sondern f&uuml;r den Start notwendig.
- * Dokumentation wird mit hilfe von Doxygen automatisch erstellt. Bitte die Dokumentation von Doxygen f&uuml;r die Kommentare beachten.
+ *
  */
 
 /*!
