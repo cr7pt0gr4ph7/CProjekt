983e7482f464f3edb056d7e5249d2caf13c5234d
hipsterfister <weissnicht05@web.de>
2013-06-04 13:49:01 +0200

    obligatorische Versionsnummer und Beschreibung

--- src/TCP.cpp
+++ src/TCP.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : TCP.cpp
 // Author      : Denis Gauss
-// Version     :
+// Version     : 1.0.0
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Spielbrett in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
