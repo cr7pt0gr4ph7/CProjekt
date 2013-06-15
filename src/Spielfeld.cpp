9d1d72bafd35ff9b13ace1bb441b5ac6eaef4f35
hipsterfister <weissnicht05@web.de>
2013-06-15 12:54:13 +0200

    Update Spielfeld.cpp
    Erweiterungen an Funktionen für die Spielinfo
    (von Hand komplett ersetzt, dank Git-Push-Error)

--- src/Spielfeld.cpp
+++ src/Spielfeld.cpp
@@ -8,11 +8,14 @@
 
 #include <iostream>
 #include "Spielfeld.h"
+#include <cstring>
 using namespace std;
 
-Spielfeld::Spielfeld(int pWidth, int pHeight) : width(pWidth), height(pHeight), spielfeld(new char[pWidth*pHeight])
+Spielfeld::Spielfeld(int pWidth, int pHeight) : width(pWidth), height(pHeight), infoWidth(20), spielfeld(new char[pWidth*pHeight]), info(new char[infoWidth*pHeight]), spielfeldinfo(new char[(pWidth+infoWidth)*pHeight])
 {
     clearSpielfeld();	//Leeres Spielfeld
+    clearInfo(); 		//Leere Info
+    initInfo();			//Initialisiere Info
 }
 
 int Spielfeld::getWidth()
@@ -27,10 +30,9 @@ int Spielfeld::getHeight()
 
 void Spielfeld::printSpielfeld()
 {
-	int x,y;
-	for(x=0;x<width;x++)
+	for(int y=0;y<height;y++)
 	{
-		for(y=0 ; y<height ; y++)
+		for(int x=0 ; x<width ; x++)
 		{
 			cout<< spielfeld[x*height+y];
 		}
@@ -45,18 +47,11 @@ void Spielfeld::drawPoint(int pX,int pY)
 }
 void Spielfeld::clearSpielfeld()
 {
-	int x,y;
-	spielfeld = new char[width*height];
-
-	for(x=0 ; x<width ; x++)//Spielfeld+Rand füllen
+	for(int y=0 ; y<height ; y++)//Spielfeld+Rand füllen
 	{
-		for(y=0 ; y<height ; y++)
+		for(int x=0 ; x<width ; x++)
 		{
-			if((x==0 || x==width-1) && (y!=0 || y!=height-1))
-			{
-				spielfeld[x*height+y]='#';
-			}
-			else if(y==0 || y==height-1)
+			if((x==0 || x==width-1) || (y==0 || y==height-1))
 			{
 				spielfeld[x*height+y]='#';
 			}
@@ -69,6 +64,67 @@ void Spielfeld::clearSpielfeld()
 	return;
 }
 
+void Spielfeld::clearInfo()
+{
+	for(int y=0;y<height;y++)
+	{
+		for(int x=0; x<width; x++)
+		{
+			info[x*height+y] = ' ';
+		}
+	}
+	return;
+}
+
+void Spielfeld::initInfo()
+{
+	addTextToInfo("Naechster Block:",1,1);
+	addTextToInfo("oooo", 3,3);
+	addTextToInfo("oooo", 3,4);
+	addTextToInfo("oooo", 3,5);
+	addTextToInfo("oooo", 3,6);
+	addTextToInfo("Spielstand:", 1,8);
+	addTextToInfo("----- Punkte",3,9);
+	addTextToInfo("  --- Linien",3,10);
+	return;
+}
+
+void Spielfeld::addInfoToSpielfeld()
+{
+	for(int y=0;y<height;y++)
+	{
+		for(int x=0; x<(width+infoWidth); x++)
+		{
+			if(x<width) spielfeldinfo[x*height+y] = spielfeld[x*height+y];
+			else spielfeldinfo[x*height+y] = info[(x-width)*height+y];
+		}
+	}
+	return;
+}
+
+void Spielfeld::addTextToInfo(const char* pStr, int pX, int pY)
+{
+	for(unsigned int x=pX;x<=(pX+strlen(pStr));x++)
+	{
+		info[x*height+pY] = pStr[x-pX];
+	}
+	return;
+}
+
+void Spielfeld::printSpielfeldInfo()
+{
+	addInfoToSpielfeld();
+
+	for(int y=0;y<height;y++)
+	{
+		for(int x=0 ; x<(infoWidth+width) ; x++)
+		{
+			cout<< spielfeldinfo[x*height+y];
+		}
+		cout<<endl;
+	}
+	return;
+}
 
 
 Spielfeld::~Spielfeld()
