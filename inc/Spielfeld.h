499784363618cbc2a42de1eba7681b7e10fdec76
hipsterfister <weissnicht05@web.de>
2013-06-15 12:55:56 +0200

    Update Spielfeld.h
    Erweiterungen an Funktionen für die Spielinfo
    (von Hand komplett ersetzt, dank Git-Push-Error)

--- inc/Spielfeld.h
+++ inc/Spielfeld.h
@@ -1,5 +1,5 @@
-#ifndef __TCP_H__
-#define __TCP_H__
+#ifndef __SPIELFELD_H__
+#define __SPIELFELD_H__
 
 
 class Spielfeld{
@@ -7,14 +7,21 @@ private:
 	int width;
 	int height;
 	int spielstand;
+	int infoWidth;
 	char *spielfeld; //als Block fuer Uebergabe an Output
 	char *info;
+	char *spielfeldinfo; //spielfeld + info
 public:
 	Spielfeld(int pWidth, int pHeight);
 	int getWidth();
 	int getHeight();
 	void clearSpielfeld(); //Spielfeld leeren
 	void printSpielfeld(); //Spielfeld ausgeben
+	void clearInfo(); //Info leeren
+	void initInfo(); //Info initialisieren
+	void addTextToInfo(const char* pStr, int pX, int pY); //Text an Punkt [X|Y] einfuegen
+	void addInfoToSpielfeld(); //Info an Spielfeldhaengen
+	void printSpielfeldInfo(); //Spielfeld mit Info ausgeben
 	void drawPoint(int pX, int pY); //Punkt im Spielfeld einfuegen
 	~Spielfeld();
 };
